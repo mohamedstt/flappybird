4fd7046103f4b61b359364a8fe1fbe9cd4620dee
mohamed schuster <mohamedschof@gmail.com>
2020-06-30 21:05:56 -0300

    Creating repository of flappy project.

---
+++ dom/js/flappy.js
@@ -0,0 +1,25 @@
+function novoElemento(tagName, className) {
+  const elem = document.createElement(tagName)
+  elem.className = className
+  return elem;
+}
+
+function Barreira(reversa = false) {
+  this.elemento = novoElemento('div', 'barreira')
+
+  const borda = novoElemento('div', 'borda')
+  const corpo = novoElemento('div', 'corpo')
+  this.elemento.appendChild(reversa ? corpo : borda)
+  this.elemento.appendChild(reversa ? borda : corpo)
+
+  this.setAltura = altura => corpo.style.height = `${altura}px` 
+}
+
+function ParDeBarreiras(altura, abertura , x) {
+  this.elemento = novoElemento('div' , 'par-de-barreiras')
+
+  this.superior = new Barreira(true)
+  this.inferio = new Barreira(false)
+
+  this.elemento.appendChild(this.superior)
+}
